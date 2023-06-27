dd5131eaa4b6aa21b63c91247ce34480b6ae4a24
Marco T O Valeriano <marco.valeriano@bancointer.com.br>
2023-06-27 16:20:06 -0300

    fix voice

--- src/app/chat/components/Answer.tsx
+++ src/app/chat/components/Answer.tsx
@@ -1,26 +1,47 @@
 "use client";
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { ConversationType } from "../page";
-import Speech from "react-text-to-speech";
+// import Speech from "react-text-to-speech";
 import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 import VolumeOffIcon from '@mui/icons-material/VolumeOff';
-export function Answer({ conversation }: { conversation: ConversationType}) {
+import { useEffect, useState } from "react";
+export function Answer({ conversation, voice }: { conversation: ConversationType; voice: SpeechSynthesisVoice | undefined}) {
+
+  const [utterance, setUtterance] = useState<SpeechSynthesisUtterance | null>(null);
+
+  useEffect(() => {
+    const synth = window.speechSynthesis;
+    const u = new SpeechSynthesisUtterance(conversation.text);
+    if(voice) u.voice = voice;
+
+    setUtterance(u);
+
+    return () => {
+      synth.cancel();
+    };
+  }, [conversation.text, voice]);
+
+  const handlePlay = () => {
+    const synth = window.speechSynthesis;
+    synth.cancel();
+
+    if(utterance){
+      synth.speak(utterance);      
+    }else {
+      synth.speak(new SpeechSynthesisUtterance(''));      
+    }
+
+  };
+
+
   return (
     <div>
       <Typography variant="body1" component={"p"} fontSize={16}>
         {conversation.author}: {conversation.text}
       </Typography>
-      <Speech
-        id="speech"
-        lang="pt-BR"
-        text={conversation.text}
-        startBtn={<VolumeOffIcon fontSize="inherit"/>} 
-        stopBtn={<VolumeUpIcon fontSize="inherit"/>} 
-        pitch={5}
-        rate={5}
-        volume={10}
-        style={{}}
-      />
+      <IconButton onClick={handlePlay}>
+        <VolumeUpIcon fontSize="inherit"/>
+      </IconButton>
     </div>
   );
 }
--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -9,7 +9,9 @@ import {
   IconButton,
   InputBase,
   InputLabel,
+  NativeSelect,
   Paper,
+  styled,
   TextField,
   Typography,
 } from "@mui/material";
@@ -19,6 +21,40 @@ import { Microphone } from "./components/Microphone";
 import CircularProgress from "@mui/material/CircularProgress";
 import { Answer } from "./components/Answer";
 import SendIcon from "@mui/icons-material/Send";
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  'label + &': {
+    marginTop: theme.spacing(3),
+  },
+  '& .MuiInputBase-input': {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}));
+
 
 type AuthorType = "USER" | "BOT";
 
@@ -36,6 +72,8 @@ export default function Chat() {
   ]);
   const [loadingGPT, setLoadingGPT] = useState<boolean>(false);
   const [currentMessage, setCurrentMessage] = useState("");
+  const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
+  const [voice, setVoice] = useState<SpeechSynthesisVoice>();
 
   const { finalTranscript } = useSpeechRecognition();
 
@@ -73,10 +111,12 @@ export default function Chat() {
   };
 
   const sendMessage = () => {
-    resetInput();
-    addConversation(currentMessage, "USER");
-    setLoadingGPT(true);
-    searchAnswerGPT(currentMessage);
+    if (currentMessage) {
+      resetInput();
+      addConversation(currentMessage, "USER");
+      setLoadingGPT(true);
+      searchAnswerGPT(currentMessage);
+    }
   };
 
   useEffect(() => {
@@ -85,14 +125,46 @@ export default function Chat() {
     }
   }, [finalTranscript, searchAnswerGPT]);
 
+  useEffect(() => {
+    if (!voices.length) {
+      const time = setInterval(() => {
+        const synth = window.speechSynthesis;
+        const voicesGetted = synth.getVoices();
+        const voicesFiltered = (voicesGetted || []).filter(
+          (voice) => voice.lang === "pt-BR"
+        );
+        setVoices(voicesFiltered);
+      }, 500);
+      return () => clearInterval(time);
+    }
+    return () => {};
+  }, [voices.length]);
+
   return (
     <div>
+      <FormControl sx={{ mb: 2 }} variant="standard" fullWidth>
+        <NativeSelect
+          value={voice?.name}
+          onChange={(e) => {
+            const newVoice = voices.find((v) => v.name === e.target.value)
+            setVoice(newVoice)
+          }}
+          input={<BootstrapInput />}
+        >
+          {voices.map((voice) => (
+            <option value={voice.name} key={voice.name}>
+              {voice.name}
+            </option>
+          ))}
+        </NativeSelect>
+      </FormControl>
+
       <Grid container>
         <Grid item xs={12}>
           {
             <Grid item display="flex" direction="column" gap="2rem">
               {conversations.map((conversation, index) => (
-                <Answer conversation={conversation} key={index} />
+                <Answer conversation={conversation} key={index} voice={voice}/>
               ))}
               {loadingGPT && <CircularProgress size={16} />}
             </Grid>
@@ -127,20 +199,28 @@ export default function Chat() {
                 p: "2px 4px",
                 display: "flex",
                 alignItems: "center",
-                width: '100%',
+                width: "100%",
               }}
             >
               <InputBase
                 sx={{ ml: 1, flex: 1 }}
                 placeholder="Digite alguma coisa para o bot responder"
-                inputProps={{ "aria-label": "digite alguma coisa para o bot responder" }}
-                value={currentMessage} onChange={(e) => changeInput(e.target.value)}
+                inputProps={{
+                  "aria-label": "digite alguma coisa para o bot responder",
+                }}
+                value={currentMessage}
+                onChange={(e) => changeInput(e.target.value)}
               />
-              <IconButton type="button" sx={{ p: "10px" }} aria-label="send message" onClick={sendMessage}>
+              <IconButton
+                type="button"
+                sx={{ p: "10px" }}
+                aria-label="send message"
+                onClick={sendMessage}
+              >
                 <SendIcon />
               </IconButton>
               <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-                <Microphone />
+              <Microphone />
             </Paper>
           </Grid>
         </Grid>
